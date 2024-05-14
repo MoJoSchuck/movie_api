d0b723dba43dec5db093d3a7376a6f6784d4d21a
Moritz Schuck <159003684+MoJoSchuck@users.noreply.github.com>
2024-05-14 09:55:08 +0200

    Updated index.js

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
 const express = require('express'),
     morgan = require('morgan'),
     uuid = require('uuid'),
@@ -26,7 +30,7 @@ const Movies = Models.Movie;
 const Users = Models.User;
 
 // mongoose.connect('mongodb://localhost:27017/cfDB');
-mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.CONNECTION_URI);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
